91581267c593340ec6aad25d0922e5c37d79043b
Kaitlyn Johnson <94390107+kaitejohnson@users.noreply.github.com>
2024-07-12 13:41:22 -0400

    edit to include spatial ww inference as pkkg name in tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(wwinference)
+library(spatialwwinference)
 
-test_check("wwinference")
+test_check("spatialwwinference")
