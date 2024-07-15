5001aaee8a9797ecae7cafbbb7ebbcf04d7fd3b7
cbernalz <cbernalz@uci.edu>
2024-07-15 14:18:42 -0700

    2024-07-15 update : added spatial unadj rt proc. func.

---
+++ R/spatial_unadj_rt_process.R
@@ -0,0 +1,45 @@
+#' Generates the subpopulation unadjusted Rt trajectories from proposal 2.
+#'
+#' @param state_rt "State" level unadjusted Rt, on log scale.
+#' @param corr_function Function for the correlation structure desired.
+#' @param corr_function_params List of parameters of desired correlation function, should contain dist matrix.
+#' @param phi_rt Coefficient for AR(1) temporal correlation on subpopulation deviations.
+#' @param sigma_eps Parameter for construction of covariance matrix of spatial epsilon.
+#'
+#' @return A matrix for unadjusted Rt where rows are subpopulations and columns are time points.
+#' 
+spatial_unadj_rt_process = function( log_state_Rt, corr_function, corr_function_params, phi_rt, sigma_eps, pre_seed = 1 ) {
+  
+  set.seed(pre_seed)
+  
+  # correlation matrix constr.
+  omega_matrix_eps = corr_function( corr_function_params )
+  sigma_matrix_eps = sigma_eps^2 * omega_matrix_eps
+  
+  # presets
+  n_subpopulations = nrow( sigma_matrix_eps )
+  n_time = length( log_state_Rt )
+  log_site_rt = matrix( data = 0, nrow = n_subpopulations, ncol = n_time )
+  delta = matrix( data = 0, nrow = n_subpopulations, ncol = n_time )
+  
+  # delta constr.
+  delta[,1] = MASS::mvrnorm( n = 1, 
+                             mu = matrix( data = 0, nrow = 1, ncol = n_subpopulations ), 
+                             Sigma = sigma_matrix_eps )
+  for (t_i in 2:n_time) {
+    eps_vec = MASS::mvrnorm( n = 1, 
+                             mu = matrix( data = 0, nrow = 1, ncol = n_subpopulations ), 
+                             Sigma = sigma_matrix_eps )
+    delta[,t_i] = phi_Rt * delta[,t_i - 1] + eps_vec
+  }
+  
+  # Subpopulation unadjusted Rt constr.
+  for (t_i in 1:n_time) {
+    log_site_rt[,t_i] = (log_state_rt[t_i] * matrix( data = 1, 
+                                                     nrow = n_subpopulations, 
+                                                     ncol = 1 )) + 
+      delta[,t_i]
+  }
+  
+  return( log_site_rt )
+}
